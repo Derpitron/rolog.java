d885a687e086272c6bd0fd227dcc1ba7876b4773
Derpitron <priya.aravamuthan@gmail.com>
2021-10-24 21:22:05 +0530

    Initial commit

---
+++ src/Rolog.java
@@ -0,0 +1,9 @@
+
+
+public class Rolog {
+    public static void main(String[] args) {
+        ProcessHandle processHandle = ProcessHandle.current();
+
+        System.out.println(processHandle.info().arguments());
+    }
+}
